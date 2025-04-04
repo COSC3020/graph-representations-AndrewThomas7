b8fd49a941ce78b24e069fbdfbfbc314a7e4a792
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-04-04 16:33:28 -0600

    Update code.js

--- code.js
+++ code.js
@@ -8,6 +8,10 @@ function convertToAdjList(adjMatrix) {
         var pushlist=[];
         var count=0;
         for(var b=0; b<adjMatrix.length; b++){
+            // var Node= adjMatrix[a];
+            // var connections =[[a+1][b+1]]
+            // Object.assign(adjMatrix,{Node,connections})
+            // console.log(adjMatrix[a][b])
             if(adjMatrix[a][b]===1&&adjMatrix.length>1){
                 pushlist.push(b+1)
                 AdjList[a+1]=pushlist
@@ -15,9 +19,6 @@ function convertToAdjList(adjMatrix) {
             }
             if(adjMatrix[a][adjMatrix.length-1]==0 && count==0){
                 AdjList[a+1]=[]
-            }
-            else{
-                AdjList[a]=[adjMatrix[a][b]]
             }
         }
     }
