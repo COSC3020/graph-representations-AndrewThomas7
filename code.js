da51f3096d6f8d76785e6cfd742deb9e8004cb80
AndrewThomas7 <168784458+AndrewThomas7@users.noreply.github.com>
2025-04-04 16:54:33 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,26 +1,28 @@
+
 function convertToAdjList(adjMatrix) {
-    var AdjList={};
-    if(adjMatrix.length<1){
-        // AdjList[0]=[]
-        return AdjList;
-    }
-    for(var a=0; a<adjMatrix.length; a++){
-        var pushlist=[];
-        var count=0;
-        for(var b=0; b<adjMatrix.length; b++){
-            // var Node= adjMatrix[a];
-            // var connections =[[a+1][b+1]]
-            // Object.assign(adjMatrix,{Node,connections})
-            // console.log(adjMatrix[a][b])
-            if(adjMatrix[a][b]===1&&adjMatrix.length>1){
-                pushlist.push(b+1)
-                AdjList[a+1]=pushlist
-                count++;
-            }
-            if(adjMatrix[a][adjMatrix.length-1]==0 && count==0){
-                AdjList[a+1]=[]
+        var AdjList=[];
+        if(adjMatrix.length<1){
+            // AdjList[0]=[]
+            return AdjList;
+        }
+        for(var a=0; a<adjMatrix.length; a++){
+            var pushlist=[];
+            var count=0;
+            for(var b=0; b<adjMatrix.length; b++){
+                // var Node= adjMatrix[a];
+                // var connections =[[a+1][b+1]]
+                // Object.assign(adjMatrix,{Node,connections})
+                // console.log(adjMatrix[a][b])
+                if(adjMatrix[a][b]===1&&adjMatrix.length>1){
+                    pushlist.push(b+1)
+                    AdjList[a]=pushlist
+                    count++;
+                }
+                if(adjMatrix[a][adjMatrix.length-1]==0 && count==0){
+                    AdjList[a]=[]
+                }
             }
         }
+        return AdjList;
     }
-    return AdjList;
-}
+
